44b863836f803eaf5baffccf40a090bd5beba472
Bryant Hughes <bryant@authenticff.com>
2016-12-23 10:01:42 -0600

    updating some stuff

--- app/Lib/Services/GoogleAnalyticsService.php
+++ app/Lib/Services/GoogleAnalyticsService.php
@@ -12,24 +12,13 @@ class GoogleAnalyticsService{
   public function __construct()
   {
 
-    $this->client = new \Google_Client();
-    $this->client->setApplicationName("BryantHughes.com");
-    $this->client->setScopes(array('https://www.googleapis.com/auth/analytics.readonly'));
-    $this->service = new \Google_Service_Analytics($this->client);
 
-    $key = file_get_contents(base_path() . "/BryantHughes-9a9ccece839d.p12");
+    $client = new \Google_Client();
+    $client->setApplicationName("BryantHughes.com");
+    $client->setScopes(array('https://www.googleapis.com/auth/analytics.readonly'));
+    $client->setDeveloperKey($this->apikey);
 
-    $cred = new \Google_Auth_AssertionCredentials(
-        "89063657226-flehl1lkgfnkpul47huhgo0sn66apifu@developer.gserviceaccount.com",
-        array(\Google_Service_Analytics::ANALYTICS_READONLY),
-        $key
-    );
-
-    $this->client->setAssertionCredentials($cred);
-    if($this->client->getAuth()->isAccessTokenExpired())
-    {
-      $this->client->getAuth()->refreshTokenWithAssertion($cred);
-    }
+    $this->service = new \Google_Service_Analytics($client);
 
   }
 
